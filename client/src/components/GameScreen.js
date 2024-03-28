d4859122cc3eed878f9d0fb9360601a81c9d78db
Isabella-Misanes <99161680+Isabella-Misanes@users.noreply.github.com>
2024-03-28 16:08:14 -0400

    Removed unused imports

--- client/src/components/GameScreen.js
+++ client/src/components/GameScreen.js
@@ -1,4 +1,4 @@
-import { Box, Button, Typography } from '@mui/material';
+import { Button, Typography } from '@mui/material';
 import { buttonStyle } from '../App';
 import * as Constants from '../constants'
 import Sidebar from './Sidebar';
