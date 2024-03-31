75e165db3277e78e71ffe9502a6de4d7f75cbccd
Isabella-Misanes <99161680+Isabella-Misanes@users.noreply.github.com>
2024-03-31 15:41:26 -0400

    Formatted HomeScreen.js

--- client/src/components/HomeScreen.js
+++ client/src/components/HomeScreen.js
@@ -4,73 +4,97 @@ import { useNavigate } from 'react-router-dom';
 
 export default function HomeScreen() {
     const navigate = useNavigate();
+
+    const homeButtons = {
+        color: 'black',
+        bgcolor: 'white',
+        ":hover": {
+            bgcolor: '#e5e5e5'},
+    }
     return (
         <div id="home-screen">
             <Sidebar />
             <Box
                 sx={{
-                    height: '100%',
-                    width: '50%',
+                    height: '95%',
+                    minWidth: '350px',
+                    width: '40%',
                     display: 'flex',
                     flexDirection: 'column',
                     paddingLeft: '2%',
+                    paddingTop: '2%',
                     backgroundColor: 'white',
                     position: 'absolute',
+                    left: '5%',
+                    top: '5%',
+                    boxShadow: '10'
                 }}>
-                <Typography variant="h2" gutterBottom>Medical Mayhem</Typography>
-                <Typography variant="h5" gutterBottom>Home Screen</Typography>
+                <Typography variant="h2" color="red" gutterBottom>Medical Mayhem</Typography>
                 
                 <Grid container spacing={4}>
-                    <Grid item xs={12}>
-                        <Button
+                    <Grid item xs={12} sx={{
+                        textAlign: 'center',
+                    }}>
+                        <Button sx={[homeButtons, {
+                            fontSize: '24pt',
+                            marginLeft: '-10%',
+                        }]}
                             onClick={()=>{navigate("/game")}}>
                             Play
                         </Button>
                     </Grid>
-                    <Grid item xs={6}>
-                        <Button
+                    <Grid item xs={6} sx={{
+                        textAlign: 'center',
+                    }}>
+                        <Button sx={homeButtons}
                             onClick={()=>{navigate("/mapsearch")}}>
                             Map Search
                         </Button>
                     </Grid>
                     <Grid item xs={6}>
-                        <Button
+                        <Button sx={homeButtons}
                             onClick={()=>{navigate("/mapbuilder")}}>
                             Map Builder
                         </Button>
                     </Grid>
-                    <Grid item xs={6}>
-                        <Button
+                    <Grid item xs={6} sx={{
+                        textAlign: 'center'
+                    }}>
+                        <Button sx={homeButtons}
                             onClick={()=>{navigate("/social")}}>
                             Social
                         </Button>
                     </Grid>
                     <Grid item xs={6}>
-                        <Button
+                        <Button sx={homeButtons}
                             onClick={()=>{navigate("/forum")}}>
                             Forums
                         </Button>
                     </Grid>
-                    <Grid item xs={6}>
-                        <Button
+                    <Grid item xs={6} sx={{
+                        textAlign: 'center',
+                    }}>
+                        <Button sx={homeButtons}
                             onClick={()=>{navigate("/profile")}}>
                             Profile
                         </Button>
                     </Grid>
                     <Grid item xs={6}>
-                        <Button
+                        <Button sx={homeButtons}
                             onClick={()=>{navigate("/settings")}}>
                             Settings
                         </Button>
                     </Grid>
-                    <Grid item xs={6}>
-                        <Button
+                    <Grid item xs={6} sx={{
+                        textAlign: 'center',
+                    }}>
+                        <Button sx={homeButtons}
                             onClick={()=>{navigate("/about")}}>
                             About
                         </Button>
                     </Grid>
                     <Grid item xs={6}>
-                        <Button
+                        <Button sx={homeButtons}
                             onClick={()=>{navigate("/leaderboard")}}>
                             Leaderboard
                         </Button>
