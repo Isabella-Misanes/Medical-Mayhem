55c92c413973f546c7fdecc6ba4a31eefaa573d6
Jared Tjahjadi <jaredtjahjadi@gmail.com>
2024-04-04 01:40:25 -0400

    minor commit to make switching between localhost and heroku baseURL easier

--- client/src/auth/auth-request-api/index.js
+++ client/src/auth/auth-request-api/index.js
@@ -5,7 +5,9 @@ axios.defaults.withCredentials = true;
 // CHANGE URL AS NEEDED BETWEEN TESTING AND DEVELOPMENT
 // TODO: Find a way to make this change between local host and Heroku 
 const api = axios.create({
-    baseURL: 'https://medical-mayhem-c0832c3f548e.herokuapp.com/auth'
+    // Uncomment one, comment out the other as needed
+    baseURL: 'http://localhost:3000/auth'
+    // baseURL: 'https://medical-mayhem-c0832c3f548e.herokuapp.com/auth'
 })
 
 // THESE ARE ALL THE REQUESTS WE`LL BE MAKING, ALL REQUESTS HAVE A
