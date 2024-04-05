fb142762d3a2e79b336dc2b4bded62b2acbaa5b6
Isabella-Misanes <99161680+Isabella-Misanes@users.noreply.github.com>
2024-04-05 12:23:12 -0400

    Make Messages Drawer

--- client/src/components/MessagesDrawer.js
+++ client/src/components/MessagesDrawer.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Box, Button, Divider, Drawer, Grid, Tab, TextField } from '@mui/material';
+import { Box, Button, Divider, Drawer, Grid, List, ListItem, Tab, TextField } from '@mui/material';
 import { TabContext, TabList, TabPanel } from '@mui/lab';
 import { useContext, useState } from 'react';
 import GlobalStoreContext from '../store';
@@ -12,7 +12,10 @@ export default function MessagesDrawer() {
     const [state, setState] = useState('bottom');
 
     const tabButton = {
-        color: '#3A9158',
+        color: 'white',
+        '&.Mui-selected': {
+            color: 'white'
+        },
     }
 
     const handleChange = (event, newValue) => {
@@ -59,15 +62,19 @@ export default function MessagesDrawer() {
             <Drawer
                 anchor={'bottom'}
                 open={state['bottom']}
-                onClose={toggleDrawer(false)}>
+                onClose={toggleDrawer(false)} 
+                sx={{
+                    width: '40%',
+                    flexShrink: 0,
+                    '& .MuiDrawer-paper': {
+                        width: '40%'
+                    }
+                }}>
                 <Box sx={{ 
-                    bgcolor: '#E7E7E7' 
+                    bgcolor: '#34732F' 
                 }}>
                     <Box sx={{ 
-                        width: '50%', 
                         typography: 'body1',
-                        pl: 2,
-                        pb: 2,
                     }}>
                         <TabContext value={value}>
                             <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
@@ -77,10 +84,111 @@ export default function MessagesDrawer() {
                                     <Tab label="Private" value="3" sx={tabButton}/>
                                 </TabList>
                             </Box>
-                            <TabPanel value="1">Public Messages</TabPanel>
-                            <TabPanel value="2">Party Messages</TabPanel>
-                            <TabPanel value="3">Private Messages</TabPanel>
-                            <Grid container spacing={1}>
+                            <TabPanel value="1">
+                                <Box sx={{
+                                    bgcolor: '#E7E7E7',
+                                }}>
+                                    <List sx={{
+                                        overflow: 'scroll',
+                                        overflowX: 'hidden',
+                                        height: '300px'
+                                    }}>
+                                        <ListItem>
+                                            <strong>McKillaGorilla</strong>: Hello World
+                                        </ListItem>
+                                        <ListItem>
+                                            <strong>ExamplePlayer</strong>: Hello!
+                                        </ListItem>
+                                        <ListItem>
+                                            <strong>MedicalGamer</strong>: Hi Guys!
+                                        </ListItem>
+                                        <ListItem>
+                                            <strong>User1</strong>: Yoooooooooo
+                                        </ListItem>
+                                        <ListItem>
+                                            <strong>User2</strong>: World
+                                        </ListItem>
+                                        <ListItem>
+                                            <strong>User3</strong>: Hi
+                                        </ListItem>
+                                        <ListItem>
+                                            <strong>User1</strong>: omg
+                                        </ListItem>
+                                        <ListItem>
+                                            <strong>ExamplePlayer</strong>: Wdym omg????
+                                        </ListItem>
+                                    </List>
+                                </Box>
+                            </TabPanel>
+                            <TabPanel value="2">
+                            <Box sx={{
+                                    bgcolor: '#E7E7E7',
+                                }}>
+                                    <List sx={{
+                                        overflow: 'scroll',
+                                        overflowX: 'hidden',
+                                        height: '300px'
+                                    }}>
+                                        <ListItem>
+                                            <strong>McKillaGorilla</strong>: Hello World
+                                        </ListItem>
+                                        <ListItem>
+                                            <strong>ExamplePlayer</strong>: Hello!
+                                        </ListItem>
+                                        <ListItem>
+                                            <strong>MedicalGamer</strong>: Hi!
+                                        </ListItem>
+                                        <ListItem>
+                                            <strong>User1</strong>: lololol
+                                        </ListItem>
+                                        <ListItem>
+                                            <strong>User1</strong>: omg
+                                        </ListItem>
+                                    </List>
+                                </Box>
+                            </TabPanel>
+                            <TabPanel value="3">
+                            <Box sx={{
+                                    bgcolor: '#E7E7E7',
+                                }}>
+                                    <List sx={{
+                                        overflow: 'scroll',
+                                        overflowX: 'hidden',
+                                        height: '300px'
+                                    }}>
+                                        <ListItem>
+                                            <strong>McKillaGorilla</strong>: Hello World
+                                        </ListItem>
+                                        <ListItem>
+                                            <strong>ExamplePlayer (You)</strong>: Hello!
+                                        </ListItem>
+                                        <ListItem>
+                                            <strong>McKillaGorilla</strong>: Hi Guys!
+                                        </ListItem>
+                                        <ListItem>
+                                            <strong>ExamplePlayer (You)</strong>: Yoooooooooo
+                                        </ListItem>
+                                        <ListItem>
+                                            <strong>McKillaGorilla</strong>: World
+                                        </ListItem>
+                                        <ListItem>
+                                            <strong>ExamplePlayer (You)</strong>: Hi
+                                        </ListItem>
+                                        <ListItem>
+                                            <strong>McKillaGorilla</strong>: omg
+                                        </ListItem>
+                                        <ListItem>
+                                            <strong>ExamplePlayer (You)</strong>: testingggg
+                                        </ListItem>
+                                    </List>
+                                </Box>
+                            </TabPanel>
+                            <Grid container spacing={1} sx={{
+                                bgcolor: '#E7E7E7',
+                                pl: 2,
+                                pr: 2,
+                                pb: 2
+                            }}>
                                 <Grid item xs={10}>
                                     <TextField fullWidth variant="standard" label="Send Message"/>
                                 </Grid>
