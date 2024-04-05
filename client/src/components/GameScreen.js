9b188d700a54d2aebfda568f99a0e3a9268e260b
Isabella-Misanes <99161680+Isabella-Misanes@users.noreply.github.com>
2024-04-05 00:50:41 -0400

    Update GameScreen.js

--- client/src/components/GameScreen.js
+++ client/src/components/GameScreen.js
@@ -1,13 +1,27 @@
-import { Typography } from '@mui/material';
+import { Box } from '@mui/material';
 import Sidebar from './Sidebar';
 import BackButton from './BackButton';
 
 export default function GameScreen() {
     return (
         <div id="about-screen">
+            <Box sx={{
+                bgcolor: 'black',
+                width: '100%',
+                height: '100%',
+                position: 'absolute'
+            }}>
+                <Box sx={{
+                    top: '5%',
+                    left: '5%',
+                    height: '90%',
+                    width: '90%',
+                    position: 'absolute',
+                    backgroundImage: 'url("https://github.com/Isabella-Misanes/cse-332-datasets/blob/ad4555391416e53e3c3dffd89ed692dcb6c11030/Welcome_Screen.png?raw=true")',
+                    backgroundSize: '100%'
+                }}/>
+            </Box>
             <Sidebar/>
-            <Typography variant="h2" gutterBottom>Medical Mayhem</Typography>
-            <Typography variant="h4" gutterBottom>Game Screen</Typography>
             <BackButton />
         </div>
     );
