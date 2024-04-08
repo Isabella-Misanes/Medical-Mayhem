787a1eb52505258150b19700a303b82f2cb6cdb7
Thomas Aloi <thomas.aloi@stonybrook.edu>
2024-04-08 12:15:27 -0400

    Breakout example works in app

--- client/src/components/GameScreen.js
+++ client/src/components/GameScreen.js
@@ -1,8 +1,21 @@
 import { Box } from '@mui/material';
 import Sidebar from './Sidebar';
 import BackButton from './BackButton';
+import { Engine } from "excalibur";
+import React from "react";
+import { intializeGame } from "../game/breakout";
 
 export default function GameScreen() {
+
+    const gameCanvas = React.useRef(null);
+	const gameRef = React.useRef();
+
+	React.useEffect(() => {
+		if (!gameRef.current && gameCanvas.current) {
+			intializeGame(gameRef, gameCanvas);
+		}
+	}, []);
+
     return (
         <div id="about-screen">
             <Box sx={{
@@ -11,7 +24,8 @@ export default function GameScreen() {
                 height: '100%',
                 position: 'absolute'
             }}>
-                <Box sx={{
+                <canvas ref={gameCanvas} id="gameCanvas"></canvas>
+                {/* <Box sx={{
                     top: '5%',
                     left: '5%',
                     height: '90%',
@@ -19,7 +33,7 @@ export default function GameScreen() {
                     position: 'absolute',
                     backgroundImage: 'url("https://github.com/Isabella-Misanes/cse-332-datasets/blob/ad4555391416e53e3c3dffd89ed692dcb6c11030/Welcome_Screen.png?raw=true")',
                     backgroundSize: '100%'
-                }}/>
+                }}/> */}
             </Box>
             <Sidebar/>
             <BackButton />
