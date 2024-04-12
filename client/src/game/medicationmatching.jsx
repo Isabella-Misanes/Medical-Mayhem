54ed3aef73c185d279541ddcd5a81f7e8cda5a80
Jared Tjahjadi <jaredtjahjadi@gmail.com>
2024-04-11 23:36:48 -0400

    Implemented medication matching on-screen text and the actual medication matching mechanism

--- client/src/game/medicationmatching.jsx
+++ client/src/game/medicationmatching.jsx
@@ -7,7 +7,15 @@
  * the patient’s box. Incorrect matching will result in that patient dying. 
  */
 
-import { Engine, Actor, Color, CollisionType, vec, Keys } from "excalibur";
+import { Engine, Actor, Color, CollisionType, vec, Keys, Text, Font, TextAlign } from "excalibur";
+
+const gameWidth = 800;
+const gameHeight = 600;
+const brickColor = [Color.Violet, Color.Orange, Color.Yellow, Color.Viridian, Color.Magenta, Color.Green, Color.Gray, Color.Vermilion, Color.Viridian];
+const randomColors = [];
+const boxCoordinates = [];
+let currCoords = {};
+let currColor = brickColor[Math.floor(Math.random() * brickColor.length)];
 
 const initializeMeds = (game) => {
   // Padding between medicines
@@ -17,22 +25,28 @@ const initializeMeds = (game) => {
   const columns = 5;
   const rows = 3;
 
-  const brickColor = [Color.Violet, Color.Orange, Color.Yellow, Color.Red, Color.Viridian, Color.Magenta, Color.Green];
-
   // Individual medicine width with padding factored in
   const brickWidth = game.drawWidth / columns - padding - padding / columns; // px
   const brickHeight = game.drawWidth / columns - padding - padding / columns; // px
   const bricks = [];
   for (let j = 0; j < rows; j++) {
+    const colCoords = [];
     for (let i = 0; i < columns; i++) {
-      bricks.push(new Actor({
+      let randomColor = brickColor[Math.floor(Math.random() * brickColor.length)];
+      randomColors.push(randomColor);
+      colCoords.push({
         x: xoffset + i * (brickWidth + padding) + padding,
-        y: yoffset + j * (brickHeight + padding) + padding,
+        y: yoffset + j * (brickHeight + padding) + padding
+      });
+      bricks.push(new Actor({
+        x: colCoords[i].x,
+        y: colCoords[i].y,
         width: brickWidth,
         height: brickHeight,
-        color: brickColor[Math.floor(Math.random() * brickColor.length)],
+        color: randomColor
       }));
     }
+    boxCoordinates.push(colCoords);
   }
 
   bricks.forEach(function (brick) {
@@ -68,41 +82,50 @@ const initializeSelector = (game) => {
 
   selectorSides.forEach(side => { game.add(side); })
 
-  // Move up/left/down/right
   let row = 0;
   let col = 0;
+  currCoords.x = xoffset + col * (brickWidth + padding) + padding;
+  currCoords.y = yoffset + row * (brickHeight + padding) + padding;
   game.input.keyboard.on('press', (event) => {
     switch(event.key) {
       case Keys.W:
         if(row !== 0) {
           selectorSides.forEach(side => {
-            side.actions.moveBy(vec(0, ((brickHeight + padding) * -1)), 1000);
+            side.actions.moveBy(vec(0, ((brickHeight + padding) * -1)), 1500);
           })
           row--;
-      }
+          currCoords.x = xoffset + col * (brickWidth + padding) + padding;
+          currCoords.y = yoffset + row * (brickHeight + padding) + padding;
+        }
         break;
       case Keys.A:
         if(col !== 0) {
           selectorSides.forEach(side => {
-            side.actions.moveBy(vec((brickHeight + padding) * -1, 0), 1000);
+            side.actions.moveBy(vec((brickHeight + padding) * -1, 0), 1500);
           })
           col--;
+          currCoords.x = xoffset + col * (brickWidth + padding) + padding;
+          currCoords.y = yoffset + row * (brickHeight + padding) + padding;
         }
         break;
       case Keys.S:
         if(row !== rows - 1) {
           selectorSides.forEach(side => {
-            side.actions.moveBy(vec(0, brickHeight + padding), 1000);
+            side.actions.moveBy(vec(0, brickHeight + padding), 1500);
           })
           row++;
+          currCoords.x = xoffset + col * (brickWidth + padding) + padding;
+          currCoords.y = yoffset + row * (brickHeight + padding) + padding;
         }
         break;
       case Keys.D:
         if(col !== columns - 1) {
           selectorSides.forEach(side => {
-            side.actions.moveBy(vec(brickHeight + padding, 0), 1000);
+            side.actions.moveBy(vec(brickHeight + padding, 0), 1500);
           })
           col++;
+          currCoords.x = xoffset + col * (brickWidth + padding) + padding;
+          currCoords.y = yoffset + row * (brickHeight + padding) + padding;
         }
         break;
       default:
@@ -113,18 +136,61 @@ const initializeSelector = (game) => {
   return selectorSides;
 }
 
+const initializeCurrColor = (game) => {
+  const currColorBorder = new Actor({
+    x: gameWidth - 100,
+    y: gameHeight - 70,
+    radius: 55,
+    color: Color.Red,
+  })
+  const currColorCircle = new Actor({
+    x: gameWidth - 100,
+    y: gameHeight - 70,
+    radius: 50,
+    color: currColor
+  })
+
+  game.add(currColorBorder);
+  game.add(currColorCircle);
+  
+  game.input.keyboard.on('press', (event) => {
+    if(event.key === Keys.E) {
+      currColorBorder.actions.moveTo(vec(currCoords.x, currCoords.y), 2000);
+      currColorCircle.actions.moveTo(vec(currCoords.x, currCoords.y), 2000);
+      currColorBorder.actions.moveTo(vec(gameWidth - 100, gameHeight - 70), 2000);
+      currColorCircle.actions.moveTo(vec(gameWidth - 100, gameHeight - 70), 2000).callMethod(() => {
+        currColor = randomColors[Math.floor(Math.random() * brickColor.length)];
+        currColorCircle.color = currColor;
+      });
+    }
+  })
+}
+
+const initializeText = (game) => {
+  const actor = new Actor({pos: vec(170, gameHeight - 30)});
+  const instrText = new Text({
+    text: 'WASD keys to select box\nE key to place medicine in box',
+    color: Color.White,
+    font: new Font({size: 24, textAlign: TextAlign.Left})
+  });
+  actor.graphics.use(instrText);
+  game.currentScene.add(actor);
+}
+
 export const initMedicationMatching = (gameRef, gameCanvasRef) => {
   if (!gameCanvasRef.current) return;
 
   gameRef.current = new Engine({
     canvasElement: gameCanvasRef.current,
-    width: 800,
-    height: 600,
+    width: gameWidth,
+    height: gameHeight,
   });
   const game = gameRef.current;
 
   initializeMeds(game);
   initializeSelector(game);
+  initializeCurrColor(game);
+  initializeText(game);
 
   game?.start().catch((e) => console.error(e));
 };
