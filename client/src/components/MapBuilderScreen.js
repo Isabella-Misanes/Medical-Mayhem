12a66114dcc4c3aca15ecf504dbd273c4202aaf2
Isabella-Misanes <99161680+Isabella-Misanes@users.noreply.github.com>
2024-04-17 19:45:05 -0400

    Included stats sliders

--- client/src/components/MapBuilderScreen.js
+++ client/src/components/MapBuilderScreen.js
@@ -1,4 +1,4 @@
-import { Grid, Paper } from '@mui/material';
+import { Grid, Paper, Slider } from '@mui/material';
 import { useContext, useState } from 'react';
 import GlobalStoreContext from '../store';
 import AuthContext from '../auth';
@@ -14,7 +14,14 @@ import player6 from '../assets/Player-6.png.png'
 export default function MapBuilderScreen() {
     const { store } = useContext(GlobalStoreContext);
     const { auth } = useContext(AuthContext);
-    const [selectedCharacter, setCharacter] = useState(null);
+    const [selectedCharacter, setCharacter] = useState(0);
+    const [postSprite, setSprite] = useState("");
+    const [speed, setSpeed] = useState(0);
+    const [strength, setStrength] = useState(0);
+    const [defense, setDefense] = useState(0);
+    const [favoredMinigame, setMinigame] = useState("");
+
+    const totalPoints = 3;
 
     const players = [
         player1,
@@ -25,9 +32,23 @@ export default function MapBuilderScreen() {
         player6
     ];
 
+    function valuetext(value) {
+        return `${value}°C`;
+    }
+
     function handleCharacterClick(image, index) {
         setCharacter(index);
     }
+
+    function handleUpdateCharacter() {
+        if(postSprite !== "") {
+            store.updateAvatar(postSprite, speed, strength, defense, favoredMinigame);
+        }
+        else {
+            store.updateAvatar(player1, speed, strength, defense, favoredMinigame);
+        }
+    }
+
     return (
         <div id="map-builder-screen">
             <Grid container spacing={2}>
@@ -38,7 +59,7 @@ export default function MapBuilderScreen() {
                     <Sidebar />
                 </Grid>
                 <Grid item xs={4}>
-                    <Grid container spacing={2}>
+                    <Grid container spacing={3}>
                         {players.map((image, index) => (
                         <Grid key={index} item xs={4}>
                             <Paper elevation={3} sx={{ 
@@ -46,7 +67,8 @@ export default function MapBuilderScreen() {
                                 width: '100%',
                                 m: 2,
                                 textAlign: 'center', 
-                                backgroundColor: selectedCharacter === index ? 'lightblue' : '#f0f0f0' 
+                                backgroundColor: selectedCharacter === index ? 'lightblue' : '#f0f0f0',
+                                border: selectedCharacter === index ? 1 : 0,
                                 }}
                                 onClick={() => handleCharacterClick(image, index)}
                             >
@@ -64,9 +86,103 @@ export default function MapBuilderScreen() {
                     ))}
                     </Grid>
                 </Grid>
-                <Grid item xs={3}>
+                <Grid item xs={1}/>
+                
+                <Grid item xs={6}>
+                    <Grid container spacing={2} sx={{
+                        alignItems: 'center',
+                    }}>
+                        <Grid item xs={12}>
+                            <h3>Character Stats</h3>
+                        </Grid>
 
+                        <Grid item xs={2}>
+                            Speed
+                        </Grid>
+                        <Grid item xs={1}>
+                            0
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Slider
+                                defaultValue={0}
+                                getAriaValueText={valuetext}
+                                valueLabelDisplay='auto'
+                                shiftStep={3}
+                                step={1}
+                                marks
+                                min={0}
+                                max={3}
+                                onChange={(event) => {setSpeed(event.target.value)}}
+                                sx={{
+                                    width: '80%'
+                                }}
+                            />
+                        </Grid>
+                        <Grid item xs={1}>
+                            3
+                        </Grid>
+                        <Grid item xs={1}/>
+
+
+                        <Grid item xs={2}>
+                            Strength
+                        </Grid>
+                        <Grid item xs={1}>
+                            0
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Slider
+                                defaultValue={0}
+                                getAriaValueText={valuetext}
+                                valueLabelDisplay='auto'
+                                shiftStep={3}
+                                step={1}
+                                marks
+                                min={0}
+                                max={3}
+                                onChange={(event) => {setStrength(event.target.value)}}
+                                sx={{
+                                    width: '80%'
+                                }}
+                            />
+                        </Grid>
+                        <Grid item xs={1}>
+                            3
+                        </Grid>
+                        <Grid item xs={1}/>
+
+
+                        <Grid item xs={2}>
+                            Defense
+                        </Grid>
+                        <Grid item xs={1}>
+                            0
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Slider
+                                defaultValue={0}
+                                getAriaValueText={valuetext}
+                                valueLabelDisplay='auto'
+                                shiftStep={3}
+                                step={1}
+                                marks
+                                min={0}
+                                max={3}
+                                onChange={(event) => {setDefense(event.target.value)}}
+                                sx={{
+                                    width: '80%'
+                                }}
+                            />
+                        </Grid>
+                        <Grid item xs={1}>
+                            3
+                        </Grid>
+                        <Grid item xs={1}/>
+
+
+                    </Grid>
                 </Grid>
+                
             </Grid>
             <BackButton/>
         </div>
