c382aa55cc9f804911e3b6c70f8feeb6ff629cd5
Isabella-Misanes <99161680+Isabella-Misanes@users.noreply.github.com>
2024-04-17 15:20:43 -0400

    MapBuilder - Upload assets and show them as grid

--- client/src/components/MapBuilderScreen.js
+++ client/src/components/MapBuilderScreen.js
@@ -1,31 +1,67 @@
-import { Box } from '@mui/material';
+import { Grid, Paper } from '@mui/material';
 import Sidebar from './Sidebar';
 import BackButton from './BackButton';
+import player1 from '../assets/Player-1.png.png'
+import player2 from '../assets/Player-2.png.png'
+import player3 from '../assets/Player-3.png.png'
+import player4 from '../assets/Player-4.png.png'
+import player5 from '../assets/Player-5.png.png'
+import player6 from '../assets/Player-6.png.png'
 
 export default function MapBuilderScreen() {
+    const players = [
+        player1,
+        player2,
+        player3,
+        player4,
+        player5,
+        player6
+    ];
+
+    function handleCharacterClick() {
+        console.log("image gotten");
+    }
     return (
         <div id="map-builder-screen">
-            <Box sx={{
-                bottom: '2.5%',
-                left: '10%',
-                height: '80%',
-                width: '80%',
-                position: 'absolute',
-                backgroundImage: 'url("https://github.com/Isabella-Misanes/cse-332-datasets/blob/main/Level_Builder.png?raw=true")',
-                backgroundSize: '100%'
-            }}/>
-            <Box sx={{
-                top: '0%',
-                left: '0%',
-                height: '90%',
-                width: '100%',
-                position: 'absolute',
-                backgroundImage: 'url("https://github.com/Isabella-Misanes/cse-332-datasets/blob/main/Toolbar.png?raw=true")',
-                backgroundSize: '100%',
-                backgroundRepeat: 'no-repeat'
-            }}/>
-            <Sidebar/>
-            <BackButton />
+            <Grid container spacing={2}>
+                <Grid item xs={11}>
+                    <h1>Build your character!</h1>
+                </Grid>
+                <Grid item xs={1}>
+                    <Sidebar />
+                </Grid>
+                <Grid item xs={4}>
+                    <Grid container spacing={2}>
+                        {players.map((image, index) => (
+                        <Grid key={index} item xs={4}>
+                            <Paper elevation={3} sx={{ 
+                                height: '100%',
+                                width: '100%',
+                                m: 2,
+                                textAlign: 'center', 
+                                backgroundColor: '#f0f0f0' 
+                                }}
+                                onClick={() => handleCharacterClick()}
+                            >
+                                <img
+                                src={image}
+                                alt={`${index+1}`}
+                                style={{
+                                    width: '100%',
+                                    height: '100%',
+                                    objectFit: 'cover'
+                                }}
+                                />
+                            </Paper>
+                        </Grid>
+                    ))}
+                    </Grid>
+                </Grid>
+                <Grid item xs={3}>
+
+                </Grid>
+            </Grid>
+            <BackButton/>
         </div>
     );
 }
