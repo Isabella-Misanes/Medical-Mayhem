9c613445d2af2c6b8bc1413b25ea4087520dde8c
Isabella-Misanes <99161680+Isabella-Misanes@users.noreply.github.com>
2024-05-03 04:30:53 -0400

    Update index.js

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from 'react'
+import { createContext, useContext, useEffect, useState } from 'react'
 import AuthContext, { UserRoleType } from '../auth';
 import apis from './store-request-api';
 // import { useNavigate } from 'react-router-dom'
