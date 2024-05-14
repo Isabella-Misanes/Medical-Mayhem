3257275b525affc9de13f64268e3fa78901c1d01
Jared Tjahjadi <jaredtjahjadi@gmail.com>
2024-05-13 23:59:27 -0400

    fixed public messages

--- client/src/store/store-request-api/index.js
+++ client/src/store/store-request-api/index.js
@@ -112,11 +112,7 @@ export const getRelationToUser = async(targetUsername) => {
 
 export const blockPlayer = async (targetUsername) => { return api.post('/blockPlayer', {targetUsername: targetUsername}); }
 
-// export const getPublicMessages = async() => {return api.get('/messages/public/get')};
-export const getPartyMessages = async() => { return api.get('/messages/party/get')};
 export const getPrivateMessages = async() => { return api.get('/messages/private/get')};
-// export const sendPublicMessage = async (username, message) => {return api.post('/messages/public/send', {username: username, message: message})}
-export const sendPartyMessage = async (message) => { return api.post('/messages/party/send', {message: message}); }
 export const sendPrivateMessage = async (message) => { return api.post('/messages/private/send', {message: message}); }
 
 const apis = {
