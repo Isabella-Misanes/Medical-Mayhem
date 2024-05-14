6bb452a5085b6cea196ac50c5c2ba7383aaf789c
Jared Tjahjadi <jaredtjahjadi@gmail.com>
2024-05-14 01:06:07 -0400

    implemented party chat

--- server/routes/message-router.ts
+++ server/routes/message-router.ts
@@ -6,11 +6,7 @@ import { MessageController } from '../controllers/message-controller';
 // Check if the user is logged in before fulfilling any authorized request
 router.use(auth.verifyToken);
 
-// router.get('/messages/public/get', MessageController.getPublicMessages)
-// router.get('/messages/party/get', MessageController.getPartyMessages)
 router.get('/messages/private/get', MessageController.getPrivateMessages)
-// router.post('/messages/public/send', MessageController.sendPublicMessage)
-// router.post('/messages/party/send', MessageController.sendPartyMessage)
 router.post('/messages/private/send', MessageController.sendPrivateMessage)
 
 export default router
