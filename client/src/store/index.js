92f2a4bedcd329ad856c31fe23ac1328f9262538
Jared Tjahjadi <jaredtjahjadi@gmail.com>
2024-05-14 15:09:47 -0400

    how tf do u private message

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -812,6 +812,10 @@ function GlobalStoreContextProvider(props) {
         asyncGetRelationToUser();
     }
 
+    store.toggleDrawer = function(open) {
+        storeReducer({ type:GlobalStoreActionType.TOGGLE_DRAWER, payload: open })
+    }
+
     store.hideModal = () => {
         storeReducer({
             type: GlobalStoreActionType.ERROR,
